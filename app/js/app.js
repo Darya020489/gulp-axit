cf38cfbd3436ba1d236ee67d7efa9f5e8a5c932c
Daria <daria7@tut.by>
2022-06-16 18:50:44 +0300

    initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,104 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Custom JS
+});
 
-	// Custom JS
+// header_has-bg=======================================================================
+let header = document.querySelector(".header");
 
-})
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
+
+// AOS init===============================================================================
+AOS.init();
+
+// MODAL-WINDOW=============================================================================
+var modal = document.getElementById("modal");
+var btn = document.getElementById("promo-btn");
+var span = document.getElementsByClassName("modal__close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// TABS=============================================================================================
+
+// Get all elements with class="tablinks" and remove the class "active"
+
+const tabs__link = document.querySelectorAll(".tabs__link");
+
+tabs__link.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+
+
+
+    function openPage(evt, pageName) {
+      // Declare all variables
+      var i, tabs__card, tabs__link;
+
+      // Get all elements with class="tabcontent" and hide them
+      tabs__card = document.getElementsByClassName("tabs__card");
+      for (i = 0; i < tabs__card.length; i++) {
+        tabs__card[i].style.display = "none";
+      }
+
+      tabs__link = document.getElementsByClassName("tabs__link");
+
+      for (i = 0; i < tabs__link.length; i++) {
+        tabs__link[i].className = tabs__link[i].className.replace(
+          " active",
+          ""
+        );
+      }
+
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(pageName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+  });
+});
+
+
+document.getElementById("defaultOpen").click();
+
+
+
+
+// THEME===========================================================================================
+document.querySelector(".switch").addEventListener("change", (event) => {
+  if (event.target.nodeName === "INPUT") {
+    document.documentElement.classList.toggle("dark");
+  }
+});
+
+// smooth-scrolling==============================================================================
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
